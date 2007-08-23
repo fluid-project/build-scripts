54dcbc840f876e00dbb223349bb0a8505ad5009d
Anastasia Cheetham <ac@anastasiacheetham.ca>
2007-08-23 13:50:12 +0000

    Updated scripts to reflect changes in the continuum setup.
    svn path=/utoronto/fluid/infrastructure/build-scripts/; revision=4344

--- sakai/reset_fluid_sakai_db.sh
+++ sakai/reset_fluid_sakai_db.sh
@@ -1,11 +1,11 @@
 #! /bin/bash -l
 
-SAKAI_TOMCAT=/home/flexibus/sakai_tomcat/
-CONTINUUM_GALLERY_PROJECT=/home/flexibus/continuum/working/12
-
+#stop tomcat
 /home/flexibus/scripts/sakai/sakai_stop_tomcat.sh
+
+#reset sakai database to known state
 mysql -u fluid_db_user sakai_fluid_db < /home/flexibus/scripts/sakai/fluid_sakai_backup.sql
-cd $CONTINUUM_GALLERY_PROJECT
-maven sakai -Dmaven.tomcat.home=$SAKAI_TOMCAT
+
+# restart tomcat
 /home/flexibus/scripts/sakai/sakai_start_tomcat.sh
 
