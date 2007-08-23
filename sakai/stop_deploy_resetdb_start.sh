8e151cf61d7ca659b5e9ed24ae063b035b2abe34
Anastasia Cheetham <ac@anastasiacheetham.ca>
2007-08-23 13:49:24 +0000

    Add new script for nightly redeploy of Fluid demo sakai.
    svn path=/utoronto/fluid/infrastructure/build-scripts/; revision=4343

---
+++ sakai/stop_deploy_resetdb_start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+source /home/flexibus/scripts/sakai/sakai_tomcat_env.sh
+CONTINUUM_GALLERY_PROJECT=/home/flexibus/continuum/working/33
+
+check_errs()
+{
+  # Function. Parameter 1 is the return code
+  # Para. 2 is text to display on failure.
+  if [ "${1}" -ne "0" ]; then
+    echo "ERROR # ${1} : ${2}"
+    exit ${1}
+  fi
+}
+
+# shut tomcat down
+/home/flexibus/scripts/sakai/sakai_stop_tomcat.sh 
+check_errs $?  "error stopping tomcat."
+
+# rebuild and redeploy the fluid components and gallery tool
+cd $CONTINUUM_GALLERY_PROJECT
+mvn -Dmaven.tomcat.home=$CATALINA_HOME -Dmaven.test.skip clean install sakai:deploy
+check_errs $?  "error rebuilding and redeploying."
+
+# reset the database to known state
+mysql -u fluid_db_user sakai_fluid_db < /home/flexibus/scripts/sakai/fluid_sakai_backup.sql
+check_errs $?  "error resetting the database."
+
+# restart tomcat
+/home/flexibus/scripts/sakai/sakai_start_tomcat.sh 
+check_errs $?  "error starting tomcat."
+
