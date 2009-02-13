16af8a57d7d8f753655cce633cb666aa40b6d837
Anastasia Cheetham <ac@anastasiacheetham.ca>
2009-02-13 20:37:32 +0000

    FLUID-1954: Remove the deployment of the gallery porlet.
    svn path=/fluid/infrastructure/build-scripts/; revision=6503

--- uportal/stop_deploy_resetdb_start.sh
+++ uportal/stop_deploy_resetdb_start.sh
@@ -24,22 +24,6 @@ echo "Step 3: update uportal to the trunk"
 /home/flexibus/scripts/uportal/uportal_update_to_trunk.sh
 check_errs $?  "error update uportal to trunk."
 
-# rebuild and install components and gallery tool
-echo "Step 4: rebuild and install components and gallery tool"
-cd $CONTINUUM_PORTLET_PROJECT
-mvn clean install
-check_errs $?  "portlet failed to rebuild and redeploy."
-
-# redeploy the portlet
-# copy the war file to an unversioned filename first, since apache needs
-# to map the path to right worker in httpd.conf, and we don't want
-# to have to restart apache for every new version
-# (see FLUID-41)
-cd $CONTINUUM_PORTLET_PROJECT/gallery-portlet/target/
-cp gallery-portlet-*.war gallery-portlet.war
-cd $UPORTAL_HOME
-ant-1.65 deployPortletApp -DportletApp=$CONTINUUM_PORTLET_PROJECT/gallery-portlet/target/gallery-portlet.war
-
 # restart tomcat
 echo "Step 5: restart tomcat"
 /home/flexibus/scripts/uportal/uportal_start_tomcat.sh >& /dev/null
