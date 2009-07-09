46aa38b5494c0fe435150d464e63d2def69a28dc
Anastasia Cheetham <ac@anastasiacheetham.ca>
2009-07-09 22:19:10 +0000

    NOJIRA: Updated scripts for Infusion and incubator daily builds.
    svn path=/fluid/infrastructure/build-scripts/; revision=7551

--- fluid/stop_deploy_start.sh
+++ fluid/stop_deploy_start.sh
@@ -22,7 +22,7 @@ ant dailyBuild
 check_errs $?  "error rebuilding"
 
 # copy the war fiel to tomcat and remove the old code
-cp $CONTINUUM_GALLERY_PROJECT/products/dist/fluid-components*.war $CATALINA_HOME/webapps/infusion.war
+cp $CONTINUUM_GALLERY_PROJECT/products/src-dist/fluid-components*.war $CATALINA_HOME/webapps/infusion.war
 rm -rf $CATALINA_HOME/webapps/infusion
 check_errs $?  "error redeploying."
 
