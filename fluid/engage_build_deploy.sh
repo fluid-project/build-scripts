5af328a29ab26cca4198906f64fb0c52535826ad
Michelle D'Souza <michelled33@gmail.com>
2009-10-08 17:33:17 +0000

    ENGAGE-116: Added error checking to the engage nightly script
    svn path=/fluid/infrastructure/build-scripts/; revision=8290

--- fluid/engage_build_deploy.sh
+++ fluid/engage_build_deploy.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 CONTINUUM_ENGAGE_PROJECT=/home/flexibus/continuum/working/56
 
-# rebuild and redeploy the fluid components 
+# check errors also exists in stop_deploy_start.sh and should be refactored
+check_errs()
+{
+  # Function. Parameter 1 is the return code
+  # Para. 2 is text to display on failure.
+  if [ "${1}" -ne "0" ]; then
+    echo "ERROR # ${1} : ${2}"
+    exit ${1}
+  fi
+}
+
+# rebuild engage with ant
 cd $CONTINUUM_ENGAGE_PROJECT/kettle/build-scripts
 ant
+check_errs $? "ant build failed"
 
 # copy the war file to tomcat and remove the old code
 cp $CONTINUUM_ENGAGE_PROJECT/kettle/build/war/target/*.war $CATALINA_HOME/webapps/engage.war
+check_errs $? "copy WAR failed"
+
 rm -rf $CATALINA_HOME/webapps/engage
 
