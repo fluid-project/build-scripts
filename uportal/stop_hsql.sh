542e038ac79c06e0512604420cca1359c1a787fe
Colin Clark <colinbdclark@gmail.com>
2011-01-14 20:54:33 +0000

    NOJIRA: committing changes to uPortal build scripts
    svn path=/fluid/infrastructure/build-scripts/; revision=10458

--- uportal/stop_hsql.sh
+++ uportal/stop_hsql.sh
@@ -2,4 +2,8 @@
 source /home/flexibus/scripts/uportal/uportal_tomcat_env.sh
 
 cd $UPORTAL_HOME
-/home/flexibus/bin/apache-ant-1.6.5/bin/ant-1.65 hsql-shutdown
+
+# for some reason ant hsql-shutdown fails, but calling hsqldb with the same parameters is successful, so commented this out
+#/home/flexibus/bin/apache-ant-1.7.1/bin/ant hsql-shutdown
+
+java -Xmx8M -classpath /home/flexibus/.m2/repository/hsqldb/hsqldb/1.8.0.10/hsqldb-1.8.0.10.jar org.hsqldb.util.SqlTool --rcFile /home/flexibus/source/uPortal/trunk/bootstrap/hsqldb.sqltool.rc --sql "SHUTDOWN compact;" uPortalDb
--- uportal/uportal_tomcat_env.sh
+++ uportal/uportal_tomcat_env.sh
@@ -1,5 +1,7 @@
+export ANT_HOME=/home/flexibus/bin/apache-ant-1.7.1
+export M2_HOME=/usr/java/apache-maven-2.2.1
 export JAVA_OPTS='-server -XX:+UseParallelGC -Xmx768m -XX:MaxPermSize=128m -Djava.awt.headless=true'
-export JAVA_HOME=/usr/java/jdk1.5.0_06
+export JAVA_HOME=/usr/lib/jvm/sun-jdk-1.6/
 export CATALINA_HOME=/home/flexibus/uportal_tomcat
 export CATALINA_PID=/home/flexibus/uportal_catalina_pid
 export UPORTAL_HOME=/home/flexibus/source/uPortal/trunk
