2c93dc19bb945464b1722c87b91ad7f346336374
Justin Obara <obarajustin@gmail.com>
2011-05-30 13:28:29 -0400

    NOJIRA: Removed the temp cleanup.
    Removed the temp directory cleanup from the script. This is causing problems with continuum because it doesn't have access to delete the files in the temp directory. The image gallery itself can take care of this already. The image gallery will have to be modified so that it doesn't complain when the temp directory already exists.

--- uploader-server/build-image-gallery.sh
+++ uploader-server/build-image-gallery.sh
@@ -5,9 +5,6 @@
 export UPLOADER_SERVER_HOME="/home/continuum/apache-continuum-1.4.0/data/working-directory/17"
 
 cd $UPLOADER_SERVER_HOME
-# remove temp directory, install.sh doesn't like it. this only applies when run manually, but still
-rm -rf infusion temp
-
 # clone infusion
  git clone https://github.com/fluid-project/infusion.git
 
