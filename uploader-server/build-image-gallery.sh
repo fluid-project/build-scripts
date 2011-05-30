90cdc2d81ba91968bca63a871423fe7bc22156d3
Justin Obara <obarajustin@gmail.com>
2011-05-30 13:19:18 -0400

    NOJIRA: modified uploader server script
    Turns out that the previous script I had written was slightly modified. It was on the server, but hadn't been committed. Adding it in now for historical reference.

--- uploader-server/build-image-gallery.sh
+++ uploader-server/build-image-gallery.sh
@@ -2,15 +2,16 @@
 
 # sets the UPLOADER_SERVER_HOME directory
 # the location where the image-gallery is checked out to
-export UPLOADER_SERVER_HOME=/home/continuum/apache-continuum-1.4.0/data/working-directory/17
+export UPLOADER_SERVER_HOME="/home/continuum/apache-continuum-1.4.0/data/working-directory/17"
 
 cd $UPLOADER_SERVER_HOME
+# remove temp directory, install.sh doesn't like it. this only applies when run manually, but still
+rm -rf infusion temp
 
 # clone infusion
  git clone https://github.com/fluid-project/infusion.git
- echo "cloning finished"
 
 # run the image-gallery's install script and set the location of infusion
-echo "./infusion" | ./install.sh
+echo "../infusion" | bash install.sh
 
 echo "end of script"
